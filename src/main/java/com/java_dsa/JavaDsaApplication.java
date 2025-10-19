58de1ac5e1f82d5d4ba8f896ea6d8394ba98fedf
unknown <venderoff@gmail.com>
2025-10-19 07:45:31 +0530

    initial

---
+++ src/main/java/com/java_dsa/JavaDsaApplication.java
@@ -0,0 +1,13 @@
+package com.java_dsa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaDsaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaDsaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/java_dsa/JavaDsaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java_dsa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaDsaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
